cadca186830c05144b4b77b0d758800a521b0334
azyobuzin <azyobuzin@users.sourceforge.jp>
2018-11-21 02:28:11 +0900

    Indexed の罠

--- RealmCompatReader/RealmTable.cs
+++ RealmCompatReader/RealmTable.cs
@@ -15,6 +15,9 @@
             this.Columns = new RealmArray(@ref.NewRef((ulong)tableArray[1]));
         }
 
+        // TODO そして注意: Indexed カラムは Columns の中で 2 要素使用する
+        // https://github.com/realm/realm-core/blob/v5.12.1/src/realm/spec.cpp#L526-L538
+
         public BpTree GetColumnBpTree(int index)
         {
             return new BpTree(this.Ref.NewRef((ulong)this.Columns[index]));
